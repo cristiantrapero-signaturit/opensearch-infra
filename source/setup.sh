32a475f144e75ccff5903d0316f1f54141792a5e
Cristian Trapero <cristian.trapero@signaturit.com>
2022-05-31 20:19:01 +0200

    cluster working

--- source/setup.sh
+++ source/setup.sh
@@ -7,7 +7,6 @@ echo "Node name: ${node_name}" >> ~/setup.log
 echo "Node role: ${node_role}" >> ~/setup.log
 echo "Cluster name: ${cluster_name}" >> ~/setup.log
 echo "Domain: ${domain}" >> ~/setup.log
-echo "Basic config" >> ~/setup.log
 
 # Persist config
 maxmap="vm.max_map_count=262144"
@@ -17,7 +16,8 @@ then
 	sysctl -p
 fi
 
-hostname "${node_name}"
+echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
+hostnamectl set-hostname "${node_name}"
 export ipaddr=$(hostname -I)
 yum install -y docker
 service docker start
@@ -31,15 +31,6 @@ docker-compose version
 
 if [[ "${node_role}" == "data" ]]
 then
-cat <<EOT > ~/opensearch.yml
-cluster.name: ${cluster_name}
-node.name: ${node_name}
-discovery.seed_hosts: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}","ops-data-1.${domain}","ops-data-2.${domain}","ops-dashboard.${domain}"]
-network.publish_host: $ipaddr
-node.roles: [ data, ingest ]
-EOT
-
-chmod 777 ~/opensearch.yml
 
 cat <<EOT > ~/docker-compose.yml
 version: '3'
@@ -48,6 +39,12 @@ services:
     image: opensearchproject/opensearch:2.0.0
     container_name: ${node_name}
     environment:
+      - cluster.name=${cluster_name}
+      - node.name=${node_name}
+      - discovery.seed_hosts=ops-master-1.${domain},ops-master-2.${domain},ops-master-3.${domain},ops-data-1.${domain},ops-data-2.${domain},ops-dashboard.${domain}
+      - cluster.initial_master_nodes=ops-master-1.${domain},ops-master-2.${domain},ops-master-3.${domain}
+      - network.publish_host=$ipaddr
+      - node.roles=data,ingest
       - bootstrap.memory_lock=true # along with the memlock settings below, disables swapping
       - "OPENSEARCH_JAVA_OPTS=-Xms512m -Xmx512m" # minimum and maximum Java heap size, recommend setting both to 50% of system RAM
     ulimits:
@@ -59,7 +56,6 @@ services:
         hard: 65536
     volumes:
       - ${node_name}:/usr/share/opensearch/data
-      - ./opensearch.yml:/usr/share/opensearch/config/opensearch.yml
     restart: unless-stopped
     ports:
       - 9200:9200
@@ -81,16 +77,6 @@ fi
 
 if [[ "${node_role}" == "master" ]]
 then
-cat <<EOT > ~/opensearch.yml
-cluster.name: ${cluster_name}
-node.name: ${node_name}
-discovery.seed_hosts: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}","ops-data-1.${domain}","ops-data-2.${domain}","ops-dashboard.${domain}"]
-network.publish_host: $ipaddr
-node.roles: [ cluster_manager ]
-EOT
-
-chmod 777 ~/opensearch.yml
-
 cat <<EOT > ~/docker-compose.yml
 version: '3'
 services:
@@ -98,6 +84,12 @@ services:
     image: opensearchproject/opensearch:2.0.0
     container_name: ${node_name}
     environment:
+      - cluster.name=${cluster_name}
+      - node.name=${node_name}
+      - discovery.seed_hosts=ops-master-1.${domain},ops-master-2.${domain},ops-master-3.${domain},ops-data-1.${domain},ops-data-2.${domain},ops-dashboard.${domain}
+      - cluster.initial_master_nodes=ops-master-1.${domain},ops-master-2.${domain},ops-master-3.${domain}
+      - network.publish_host=$ipaddr
+      - node.roles=cluster_manager
       - bootstrap.memory_lock=true # along with the memlock settings below, disables swapping
       - "OPENSEARCH_JAVA_OPTS=-Xms512m -Xmx512m" # minimum and maximum Java heap size, recommend setting both to 50% of system RAM
     ulimits:
@@ -109,7 +101,6 @@ services:
         hard: 65536
     volumes:
       - ${node_name}:/usr/share/opensearch/data
-      - ./opensearch.yml:/usr/share/opensearch/config/opensearch.yml
     restart: unless-stopped
     ports:
       - 9200:9200
@@ -142,39 +133,6 @@ fi
 
 if [ "${node_role}" == "dashboard" ]
 then
-cat <<EOT > ~/opensearch_dashboards.yml
-# Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License").
-# You may not use this file except in compliance with the License.
-# A copy of the License is located at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# or in the "license" file accompanying this file. This file is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
-# express or implied. See the License for the specific language governing
-# permissions and limitations under the License.
-
-# Description:
-# Default configuration for OpenSearch Dashboards
-
-opensearch.hosts: ["https://ops-master-1.${domain}:9200","https://ops-master-2.${domain}:9200","https://ops-master-3.${domain}:9200"]
-opensearch.ssl.verificationMode: none
-opensearch.username: "admin"
-opensearch.password: "adminadmin"
-opensearch.requestHeadersWhitelist: [ authorization,securitytenant ]
-
-opensearch_security.multitenancy.enabled: true
-opensearch_security.multitenancy.tenants.preferred: ["Private", "Global"]
-opensearch_security.readonly_mode.roles: ["kibana_read_only"]
-# Use this setting if you are running kibana without https
-opensearch_security.cookie.secure: false
-server.host: ${node_name}
-EOT
-
-chmod 777 ~/opensearch_dashboards.yml
-
 cat <<EOT > ~/docker-compose.yml
 version: '3'
 services:
@@ -184,7 +142,9 @@ services:
     restart: unless-stopped
     volumes:
       - ${node_name}:/usr/share/opensearch/data
-      - ./opensearch_dashboards.yml:/usr/share/opensearch-dashboards/config/opensearch_dashboards.yml
+    environment:
+      PLUGINS_SECURITY_DISABLED: "true"
+      OPENSEARCH_HOSTS: '["https://ops-master-1.opensearch.local:9200","https://ops-master-2.opensearch.local:9200","https://ops-master-3.opensearch.local:9200"]'
     ports:
       - 5601:5601
     expose:
