d40298f1bf4a5acf805caad3a968952b5aa6ccf3
Cristian Trapero <cristian.trapero@signaturit.com>
2022-05-31 08:55:16 +0200

    change instance type. add docker-compose.yml

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 module "opensearch" {
   source = "./modules/opensearch"
   cluster_name = "opensearch-cluster"
-  create_vpc = true
+  create_vpc = false
   stack = "opensearch"
-  sg_vpc_id = ["sg-0b90f9b50f45b306c"]
-  subnet_id = "subnet-00229e79ee24eac6e"
-  route53_zone = "Z0201849OCAUBDDSBU8Q"
-  route53_domain = "cristiantrapero.com"
+  sg_vpc_id = ["sg-066c895752d9cb6d8"]
+  subnet_id = "subnet-08ed6593932ffb22e"
+  route53_zone = "Z09247162ZMZ5VPJULBQF"
+  route53_domain = "opensearch.local"
   path_to_data = "/tmp/data"
-  cidr_block  = "10.0.0.0/16"
+  cidr_block  = "10.0.0.0/20"
   subnet_availability_zones = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
   region = "eu-west-1"
 }
--- source/setup.sh
+++ source/setup.sh
@@ -16,12 +16,25 @@ echo "Cluster name: ${cluster_name}" >> ~/setup.log
 echo "Domain: ${domain}" >> ~/setup.log
 echo "Path to data: ${path_to_data}" >> ~/setup.log
 echo "Basic config" >> ~/setup.log
-sysctl -w vm.max_map_count=262144
+
+maxmap="vm.max_map_count=262144"
+if ! test "$(grep $maxmap /etc/sysctl.conf)"
+then
+	echo "$maxmap" >> /etc/sysctl.conf
+	sysctl -p
+fi
+
 hostname "${node_name}"
 export ipaddr=$(hostname -I)
 yum install -y docker
 service docker start
-service docker enable
+systemctl enable docker
+
+# Install docker-compose
+curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose version
 
 if [[ "${node_role}" == "data" ]]
 then
@@ -29,16 +42,47 @@ cat <<EOT > ~/opensearch.yml
 cluster.name: ${cluster_name}
 node.name: ${node_name}
 discovery.seed_hosts: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}","ops-data-1.${domain}","ops-data-2.${domain}","ops-dashboard.${domain}"]
-cluster.initial_master_nodes: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}"]
-network.host: $ipaddr
-path.data: ${path_to_data}
+network.host: [_local_, _site_]
+node.roles: [ data, ingest ]
 EOT
 
-    if [[ "${node_name}" == "ops-master-1" ]]
-    then
-        # Create the certificates
-        ./generate-certificates.sh
-    fi
+chmod 777 ~/opensearch.yml
+
+cat <<EOT > ~/docker-compose.yml
+version: '3'
+services:
+  ${node_name}:
+    image: opensearchproject/opensearch:2.0.0
+    container_name: ${node_name}
+    environment:
+      - bootstrap.memory_lock=true # along with the memlock settings below, disables swapping
+      - "OPENSEARCH_JAVA_OPTS=-Xms512m -Xmx512m" # minimum and maximum Java heap size, recommend setting both to 50% of system RAM
+    ulimits:
+      memlock:
+        soft: -1
+        hard: -1
+      nofile:
+        soft: 65536 # maximum number of open files for the OpenSearch user, set to at least 65536 on modern systems
+        hard: 65536
+    volumes:
+      - ${node_name}:/usr/share/opensearch/data
+      - ./opensearch.yml:/usr/share/opensearch/config/opensearch.yml
+    restart: unless-stopped
+    ports:
+      - 9200:9200
+      - 9600:9600 # required for Performance Analyzer
+    networks:
+      - opensearch-net
+
+volumes:
+  ${node_name}:
+
+networks:
+  opensearch-net:
+EOT
+  echo "Start docker compose service" >> ~/setup.log
+  docker-compose -f ~/docker-compose.yml up -d
+  echo "Start docker compose service...Done" >> ~/setup.log
 fi
 
 if [[ "${node_role}" == "master" ]]
@@ -47,17 +91,58 @@ cat <<EOT > ~/opensearch.yml
 cluster.name: ${cluster_name}
 node.name: ${node_name}
 discovery.seed_hosts: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}","ops-data-1.${domain}","ops-data-2.${domain}","ops-dashboard.${domain}"]
-cluster.initial_master_nodes: ["ops-master-1.${domain}","ops-master-2.${domain}","ops-master-3.${domain}"]
-network.host: $ipaddr
-path.data: ${path_to_data}
+network.host: [_local_, _site_]
+node.roles: [ cluster_manager ]
 EOT
+
+chmod 777 ~/opensearch.yml
+
+cat <<EOT > ~/docker-compose.yml
+version: '3'
+services:
+  ${node_name}:
+    image: opensearchproject/opensearch:2.0.0
+    container_name: ${node_name}
+    environment:
+      - bootstrap.memory_lock=true # along with the memlock settings below, disables swapping
+      - "OPENSEARCH_JAVA_OPTS=-Xms512m -Xmx512m" # minimum and maximum Java heap size, recommend setting both to 50% of system RAM
+    ulimits:
+      memlock:
+        soft: -1
+        hard: -1
+      nofile:
+        soft: 65536 # maximum number of open files for the OpenSearch user, set to at least 65536 on modern systems
+        hard: 65536
+    volumes:
+      - ${node_name}:/usr/share/opensearch/data
+      - ./opensearch.yml:/usr/share/opensearch/config/opensearch.yml
+    restart: unless-stopped
+    ports:
+      - 9200:9200
+      - 9600:9600 # required for Performance Analyzer
+    networks:
+      - opensearch-net
+
+volumes:
+  ${node_name}:
+
+networks:
+  opensearch-net:
+EOT
+
 
     if [[ "${node_name}" == "ops-master-1.${domain}" ]]
     then
         # Create the certificates
-        #./generate-certificates.sh
         echo "Generating certificates..." >> ~/setup.log
+        #./generate-certificates.sh
+        echo "Generating certificates... Done" >> ~/setup.log
+        echo "Distribute certificates to nodes" >> ~/setup.log
     fi
+
+  echo "Start docker compose service" >> ~/setup.log
+  docker-compose -f ~/docker-compose.yml up -d
+  echo "Start docker compose service...Done" >> ~/setup.log
 fi
 
 if [ "${node_role}" == "dashboard" ]
@@ -82,7 +167,7 @@ cat <<EOT > ~/opensearch_dashboards.yml
 opensearch.hosts: ["https://ops-master-1.${domain}:9200","https://ops-master-2.${domain}:9200","https://ops-master-3.${domain}:9200"]
 opensearch.ssl.verificationMode: none
 opensearch.username: "admin"
-opensearch.password: "admin"
+opensearch.password: "adminadmin"
 opensearch.requestHeadersWhitelist: [ authorization,securitytenant ]
 
 opensearch_security.multitenancy.enabled: true
@@ -92,4 +177,35 @@ opensearch_security.readonly_mode.roles: ["kibana_read_only"]
 opensearch_security.cookie.secure: false
 server.host: ${node_name}
 EOT
+
+chmod 777 ~/opensearch_dashboards.yml
+
+cat <<EOT > ~/docker-compose.yml
+version: '3'
+services:
+  ${node_name}:
+    image: opensearchproject/opensearch-dashboards:2.0.0
+    container_name: ${node_name}
+    restart: always
+    volumes:
+      - ${node_name}:/usr/share/opensearch/data
+      - ./opensearch_dashboards.yml:/usr/share/opensearch-dashboards/config/opensearch_dashboards.yml
+    ports:
+      - 5601:5601
+    expose:
+      - "5601"
+    networks:
+      - opensearch-net
+
+volumes:
+  ${node_name}:
+
+networks:
+  opensearch-net:
+EOT
+
+  echo "Start docker compose service" >> ~/setup.log
+  docker-compose -f ~/docker-compose.yml up -d
+  echo "Start docker compose service...Done" >> ~/setup.log
+
 fi
